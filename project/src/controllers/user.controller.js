119cea11e02e586f62baf7c6d2489fd88324053e
Deba-07 <dynamicdeba07@gmail.com>
2024-12-26 09:31:58 +0530

    register controller bugs fixed

--- project/src/controllers/user.controller.js
+++ project/src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   //check if user already exists: username, email
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
@@ -26,9 +26,13 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //check for images and avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req?.files?.coverimage[0]?.path;
+  //const coverImageLocalPath = req?.files?.coverImage[0]?.path;
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
+  }
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    coverImageLocalPath = req.files.coverImage[0].path
   }
 
   //upload on cloudinary
@@ -49,7 +53,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   //remove password and refreshToken field from response
-  const createdUser = await User.findById(user._id).select("-password - refreshToken")
+  const createdUser = await User.findById(user._id).select("-password -refreshToken")
 
   //check for user creation
   if(!createdUser){
